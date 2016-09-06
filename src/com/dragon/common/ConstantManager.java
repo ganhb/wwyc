2a943bf5daecca6d0f697cfc7c2e9e7e09679542
ganhb <ghblink@163.com>
2016-09-06 09:32:19 +0800

    Update ConstantManager.java

--- src/com/dragon/common/ConstantManager.java
+++ src/com/dragon/common/ConstantManager.java
@@ -8,6 +8,6 @@ package com.dragon.common;
  */
 
 public class ConstantManager {
-	public static final String APPID = "wxa03d062f5ae74167";
-	public static final String SECRET = "072ca0cabb79357c5c2421a25902ec4a";
+	public static final String APPID = "xxxxx";
+	public static final String SECRET = "xxxxxxx";
 }
